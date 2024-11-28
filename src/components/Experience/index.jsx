90c22bd874513f34b941c75b6419e2cd459c64cc
Rashi <rashi1704baranwal@gmail.com>
2024-11-28 12:08:24 +0530

    fix: experience and education description modified

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -21,11 +21,11 @@ const Experience = () => {
                 {" "}
                 GPA: 8.92 <br />
                 Branch : CSE Core <br />
-                <br />
+                {/* <br />
                 • Pursuing a degree in Computer Science with a focus on software
                 development, algorithms, and web technologies. Involved in
                 various technical clubs, projects, and internships to enhance
-                practical knowledge in the field.
+                practical knowledge in the field. */}
               </p>
             </div>
             <div className="box">
@@ -35,11 +35,13 @@ const Experience = () => {
                 Grade : 94% <br />
                 Activities and societies : Music ,Dance, Volleyball , Basketball
                 and Coding.
-                <br /> <br />
-                • Completed Higher Secondary education with a specialization in
+                <br />
+                Subjects: PCM with Economics & Dance(Kathak)
+                <br />
+                {/*• Completed Higher Secondary education with a specialization in
                 Science. Developed a strong foundation in PCM with Economics &
                 Dance(Kathak).Participation in coding competitions and school
-                projects.
+                projects. */}
               </p>
             </div>
             <div className="box">
@@ -47,12 +49,15 @@ const Experience = () => {
               <h3>Apr 2008 - Apr 2020</h3>
               <p>
                 Grade : 94.4% <br />
-                Activities and societies : Dancing, Music & Basketball.
+                Subjects: Physics, Chemistry, Biology, Maths, English, Hindi,
+                Commercial Application, History and Geography.
                 <br />
+                Activities and societies : Dancing, Music & Basketball.
+                {/* <br />
                 <br />
                 • Built a solid academic foundation in core subjects. Actively
                 engaged in extracurricular activities, laying the groundwork for
-                a passion in technology and coding.
+                a passion in technology and coding. */}
               </p>
             </div>
           </div>
@@ -68,21 +73,17 @@ const Experience = () => {
               <h4>EVOLVEDEV</h4>
               <h3>FULL STACK DEVELOPER (Jun 2024 - Now)</h3>
               <p>
-                <br />
-                • Designed and launched an optimized landing page with
-                Figma,React JS and Tailwind CSS, implementing a contact form
-                that streamlined user inquiries; this led to a 40% increase in
-                lead generation within two months. <br /> <br />• Executed responsive
-                design and interactive features, enhancing 30% user experience
-                and site functionality.
+                <br />• Created an optimized landing page with Figma, React JS,
+                and Tailwind CSS, increasing leads by 40% and enhancing user
+                experience by 30%.
               </p>
             </div>
             <div className="box">
               <h4>BLOCKCHAIN CLUB</h4>
               <h3>Technical Team Lead (Nov 2023 - Now)</h3>
               <p>
-                <br />
-                • Leading the technical team in the Blockchain Club, overseeing project development and implementation. Engaged in innovative blockchain-based projects to contribute to the tech community at VIT Bhopal.
+                <br />• Leading the Blockchain Club's technical team, driving
+                blockchain projects and fostering innovation at VIT Bhopal.
               </p>
             </div>
             <div className="box">
@@ -90,10 +91,9 @@ const Experience = () => {
               <h3>Technical Team Core Member (Aug 2023 - Apr 2024)</h3>
               <p>
                 <br />
-                • Played a key role as a core member of the technical team. Contributed to various tech projects, assisting in ideation, development, and problem-solving across multiple domains.
-                <br /><br />
-                • Successfully organised EDGETRONIX workshop, captivating an audience of 200 participants. Managed and volunteered the execution of the Arduino Demo and conducted quiz.
-                
+                Contributed to tech projects as a core team member and organized
+                the EDGETRONIX workshop for 200 participants, leading Arduino
+                demos and quizzes.
               </p>
             </div>
           </div>
